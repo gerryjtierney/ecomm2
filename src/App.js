c9e6650642c48d13bdb37a5c5c50601bbc2724c2
gerryjtierney <gerryjtierney@gmail.com>
2021-11-03 16:47:57 +0000

    updated navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
+import { Route } from "react-router-dom";
 import HomePage from './pages/homepage/homepage.component';
 import "../src/pages/homepage/homepage.styles.css";
+
+function HatsPage(){
+  return(
+  <div>
+    <h1>HATS</h1>
+  </div>
+  )
+};
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+
+
     </div>
   );
 }
